215fad79a03148a07d0214f53ced39400d6ad20a
raghulprashath <raghulprashath@gmail.com>
2022-11-28 14:12:02 +0530

    Test3

--- src/Media.js
+++ src/Media.js
@@ -21,6 +21,7 @@ function Media() {
             <div className="bgslider3">
                 <div className="maincontent3"><font><li>Participants who wish to apply for multiple categories are encouraged to register for each category separately.</li></font></div>
                 <div className="maincontent3"><font><li>Please note that the number of categories applied to will not increase or decrease the chance of getting in.</li></font></div>
+                <div className="maincontent3"><font><li>All years are open to apply for the auditions.</li></font></div>
                 <div className="maincontent3"><font><li>It will be purely based on the skills of the applicant in the respective category.</li></font></div>
                 <div className="maincontent3"><font><li>Keep in mind that a well recorded video/audio will be easier to judge.</li></font></div>
                 <div className="maincontent3"><font><li>While uploading the audition clip, please make sure that you name the file you’re uploading in the specified format to avoid confusion “RollNumber_Category” (for eg: 22XX40_Keyboard)</li></font></div>
